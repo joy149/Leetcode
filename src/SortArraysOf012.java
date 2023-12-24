cce5414e0c4c2594702b1161fea0c7e32109fa64
joy149 <jbhwmck149@gmail.com>
2023-12-24 23:16:44 +0530

    Optimizing using dutch national flag algo 12/24

--- src/SortArraysOf012.java
+++ src/SortArraysOf012.java
@@ -5,6 +5,8 @@ import java.util.stream.Collectors;
 public class SortArraysOf012 {
 
     //TODO: To find better solution than this
+
+    //This solution has a time complexity of O(2N)
     public void sortColors(int[] nums) {
         Map<Integer, Integer> countMap = new HashMap<>();
         for (int i = 0; i<nums.length; i++) {
@@ -34,7 +36,42 @@ public class SortArraysOf012 {
 
     }
 
+    //Dutch National Flag Algo
+    /*
+    Follow the link for detailed explanation
+    https://takeuforward.org/data-structure/sort-an-array-of-0s-1s-and-2s/
+
+    if arr[mid] is 0 swap arr[low] and arr[mid] and do mid++ , low++
+    if arr[mid] is 1 do mid++
+    if arr[mid] is 2 swap arr[mid] with arr[high] and high--
+     */
+    public void sortColorsOptimized(int[] arr) {
+
+        int low = 0, mid = 0;
+        int high = arr.length - 1;
+
+        while (mid <= high){
+            if (arr[mid] == 0) {
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+                low++;
+                mid++;
+            }
+            else if (arr[mid] == 2) {
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                high--;
+            }
+            else {
+                mid++;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
     public static void main(String[] args) {
-        (new SortArraysOf012()).sortColors(new int[]{1,2});
+        (new SortArraysOf012()).sortColorsOptimized(new int[]{2,0,2,1,1,0});
     }
 }
